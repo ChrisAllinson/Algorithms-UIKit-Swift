f31fe8aceb50ac5cbc3c0def5f709801ca7984d8
Chris Allinson <allinson.ca@hotmail.com>
2024-01-17 22:02:20 -0700

    Added factorial algorithms
    - factorial using iteration
    - factorial using recursion

---
+++ Algorithms-UIKit-Swift/Algorithms-UIKit-Swift/Algorithms/Factorial/FactorialBaseViewController.swift
@@ -0,0 +1,61 @@
+//
+//  FactorialBaseViewController.swift
+//  Algorithms-UIKit-Swift
+//
+//  Created by Chris Allinson on 2024-01-17.
+//
+
+import UIKit
+
+protocol FactorialManagable {
+    func calculateFactorial(of inputValue: UInt) -> UInt
+}
+
+class FactorialBaseViewController: UIViewController {
+    
+    // MARK: instance properties
+    
+    var viewModel: FactorialManagable!
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet private var selectValueLabel: UILabel!
+    @IBOutlet private var valueSlider: UISlider!
+    @IBOutlet private var calculationLabel: UILabel!
+    
+    // MARK: lifecycle methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        selectValueLabel.text = String(localized: "Select_A_Value")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setViewControllerBackButton(title: "Back")
+    }
+    
+    // MARK: IBActions
+    
+    @IBAction private func slideValueUpdated(sender: UISlider) {
+        let roundedSliderValue = round(sender.value)
+        let intSliderValue = UInt(roundedSliderValue)
+        
+        updateSliderValue(sliderValue: intSliderValue)
+        
+        let calculation = viewModel.calculateFactorial(of: intSliderValue)
+        setCalculationLabel(sliderValue: intSliderValue, calculation: calculation)
+    }
+    
+    // MARK: private methods
+    
+    private func updateSliderValue(sliderValue: UInt) {
+        valueSlider.value = Float(sliderValue)
+    }
+    
+    private func setCalculationLabel(sliderValue: UInt, calculation: UInt) {
+        calculationLabel.text = "\(sliderValue)" + "! = " + "\(calculation)"
+    }
+}
--- Algorithms-UIKit-Swift/Algorithms-UIKit-Swift/Application/SceneDelegate.swift
+++ Algorithms-UIKit-Swift/Algorithms-UIKit-Swift/Application/SceneDelegate.swift
@@ -9,8 +9,11 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+    // MARK: instance properties
+    
     var window: UIWindow?
 
+    // MARK: UIWindowSceneDelegate methods
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -46,7 +49,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
